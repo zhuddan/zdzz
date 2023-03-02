40d8aebb30ffe44458fac4399e5c3bd3333e6ff3
zhuddan <476182045@qq.com>
2023-03-02 14:57:42 +0800

    update

--- packages/shared/vite.config.ts
+++ packages/shared/vite.config.ts
@@ -1,50 +1,6 @@
+import { packagesConfig } from '../../script/config';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig(() => {
-  return {
-    plugins: [
-      dts({
-        include: ['src/**/*.ts'],
-        outputDir: 'dist/types',
-      }),
-    ],
-    build: {
-      target: 'modules',
-      // 打包文件目录
-      outDir: 'dist/es',
-      // 压缩
-      minify: true,
-      // css分离
-      // cssCodeSplit: true,
-      rollupOptions: {
-        input: ['src/index.ts'],
-        output: [
-          {
-            format: 'es',
-            // 不用打包成.es.js,这里我们想把它打包成.js
-            entryFileNames: '[name].js',
-            // 让打包目录和我们目录对应
-            preserveModules: true,
-            // 配置打包根目录
-            dir: 'dist/es',
-            preserveModulesRoot: 'src',
-          },
-          {
-            format: 'cjs',
-            entryFileNames: '[name].js',
-            // 让打包目录和我们目录对应
-            preserveModules: true,
-            // 配置打包根目录
-            dir: 'dist/lib',
-            preserveModulesRoot: 'src',
-          },
-        ],
-      },
-      lib: {
-        entry: './src/index.ts',
-      },
-    },
-  };
+  return packagesConfig;
 });
--- script/config.ts
+++ script/config.ts
@@ -1,3 +1,74 @@
-import path from 'path';
-const s = path.resolve(__dirname);
-console.log(s);
+import type { UserConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export const packagesConfig: UserConfig = {
+  plugins: [
+    dts({
+      include: ['src/**/*.ts'],
+      outputDir: ['dist', 'lib', 'es'],
+    }),
+  ],
+  build: {
+    target: 'es2015',
+    outDir: 'dist',
+    minify: false,
+    chunkSizeWarningLimit: 1000,
+    lib: {
+      entry: './src/index.ts',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: [
+        {
+          globals: {
+            vue: 'Vue',
+          },
+          format: 'es',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          dir: 'es',
+          preserveModulesRoot: 'src',
+        },
+        {
+          globals: {
+            vue: 'Vue',
+          },
+          format: 'cjs',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          dir: 'lib',
+          preserveModulesRoot: 'src',
+        },
+        // dist
+        {
+          globals: {
+            vue: 'Vue',
+          },
+          format: 'iife',
+          entryFileNames: '[name].js',
+          dir: 'dist',
+          name: 'zd',
+        },
+        {
+          globals: {
+            vue: 'Vue',
+          },
+          format: 'es',
+          entryFileNames: '[name].es.js',
+          dir: 'dist',
+          preserveModulesRoot: 'src',
+        },
+        {
+          globals: {
+            vue: 'Vue',
+          },
+          format: 'cjs',
+          entryFileNames: '[name].cjs.js',
+          dir: 'dist',
+          preserveModulesRoot: 'src',
+        },
+      ],
+    },
+  },
+};
+export default packagesConfig;
