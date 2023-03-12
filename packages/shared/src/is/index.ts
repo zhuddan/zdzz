4f8fca3377375626346641c33add2836a614660f
zhuddan <476182045@qq.com>
2023-03-12 19:07:39 +0800

    update

--- packages/shared/src/is/index.ts
+++ packages/shared/src/is/index.ts
@@ -4,12 +4,8 @@ export function is(val: unknown, type: string) {
   return toString.call(val) === `[object ${type}]`;
 }
 
-export function isDef<T = unknown>(val?: T): val is T {
-  return typeof val !== 'undefined';
-}
-
-export function isUnDef<T = unknown>(val?: T): val is T {
-  return !isDef(val);
+export function isUnDef<T = any>(val?: T): val is undefined {
+  return val === undefined;
 }
 
 export function isObject(val: any): val is Record<any, any> {
@@ -35,10 +31,6 @@ export function isDate(val: unknown): val is Date {
 
 export function isNull(val: unknown): val is null {
   return val === null;
-}
-
-export function isNullAndUnDef(val: unknown): val is null | undefined {
-  return isUnDef(val) && isNull(val);
 }
 
 export function isNullOrUnDef(val: unknown): val is null | undefined {
