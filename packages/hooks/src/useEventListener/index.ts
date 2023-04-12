c13314af5fbdf2fcaf9212df094ce3d7afde525b
zhuddan <476182045@qq.com>
2023-04-12 11:13:42 +0800

    fix(useEventListener): type

--- packages/hooks/src/useEventListener/index.ts
+++ packages/hooks/src/useEventListener/index.ts
@@ -1,18 +1,28 @@
 import { onMounted, onUnmounted, unref } from 'vue';
 
-type MaybeElement = ElRef | HTMLElement | string | (Window & typeof globalThis);
+type WindowType = Window & typeof globalThis;
+type WindowEvent = keyof WindowEventMap;
+type HTMLElementEvent = keyof HTMLElementEventMap;
+type MaybeElement = ElRef | HTMLElement | string | WindowType;
 
-export function useEventListener(target: MaybeElement, event: string, callback: Fn, immediate = false) {
-  function getTarget() {
+export function useEventListener<T extends MaybeElement>(
+  target: T,
+  event: T extends WindowType ? WindowEvent : HTMLElementEvent,
+  callback: Fn,
+  immediate = false,
+) {
+  function getTarget(): HTMLElement | WindowType | Element {
     if (target instanceof HTMLElement)
       return target;
-
     if (typeof target == 'string')
-      return document.querySelector(target);
-
-    return unref(target);
+      return document.querySelector(target)!;
+    return unref(target) as HTMLElement;
   }
+  onMounted(() => {
+    getTarget()?.addEventListener(event, callback);
+  });
+  onUnmounted(() => {
+    getTarget()?.removeEventListener(event, callback);
+  });
   immediate && callback();
-  onMounted(() => getTarget()?.addEventListener(event, callback));
-  onUnmounted(() => getTarget()?.removeEventListener(event, callback));
 }
