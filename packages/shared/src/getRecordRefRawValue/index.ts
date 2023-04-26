281014fad2a6b98ca9980f7f37d0cfa6c43fff9a
zhuddan <476182045@qq.com>
2023-04-26 10:37:05 +0800

    update

--- packages/shared/src/getRecordRefRawValue/index.ts
+++ packages/shared/src/getRecordRefRawValue/index.ts
@@ -1,12 +1,11 @@
-import { unref } from 'vue';
+import { toRaw, unref } from 'vue';
 
 export function getRecordRefRawValue<T extends Recordable>(maybeShallowRecordRef: MaybeShallowRecordRef<T>): T {
   const res = {} as T;
   const keys = Object.keys(maybeShallowRecordRef);
   for (let index = 0; index < keys.length; index++) {
     const key = keys[index] as keyof T;
-    res[key] = unref(maybeShallowRecordRef[key]) as any;
+    res[key] = toRaw(unref(maybeShallowRecordRef[key])) as any;
   }
   return res;
 }
-
