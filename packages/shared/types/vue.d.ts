329411866e4b0d55fc7cabc955df709636760bd7
zhuddan <476182045@qq.com>
2023-05-02 11:21:56 +0800

    update

--- packages/shared/types/vue.d.ts
+++ packages/shared/types/vue.d.ts
@@ -2,18 +2,18 @@ import type { ComputedRef, Ref, PropType as VuePropType } from 'vue';
 
 declare global{
 
-  declare type PropType<T> = VuePropType<T>;
+  type PropType<T> = VuePropType<T>;
 
-  declare type EmitType = (event: string, ...args: any[]) => void;
+  type EmitType = (event: string, ...args: any[]) => void;
 
-  declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
+  type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
   // 解决类型嵌套过深的问题
-  // declare type MaybeRef<T> = T | Ref<T>;
-  declare type MaybeRef<T> = Ref<UnwrapRef<T>> | T;
+  // type MaybeRef<T> = T | Ref<T>;
+  type MaybeRef<T> = Ref<UnwrapRef<T>> | T;
 
-  declare type MaybeComputedRef<T> = MaybeReadonlyRef<T> | MaybeRef<T>;
+  type MaybeComputedRef<T> = MaybeReadonlyRef<T> | MaybeRef<T>;
 
-  declare type MaybeReadonlyRef<T> = (() => T) | ComputedRef<T>;
+  type MaybeReadonlyRef<T> = (() => T) | ComputedRef<T>;
 
   /**
    * @description 浅层的RecordRef
@@ -33,22 +33,23 @@ declare global{
    *    age: 1, //
    * }
    */
-  declare type MaybeShallowRecordRef<T extends Recordable> = {
+  type MaybeShallowRecordRef<T extends Recordable> = {
     [P in keyof T]: MaybeRef<T[P]>;
   };
   /**
    * @description 深层的RecordRef
    */
-  declare type MaybeRecordRef<T extends Recordable> = {
+  type MaybeRecordRef<T extends Recordable> = {
     [P in keyof T]: T[P] extends Recordable ? MaybeRecordRef<T[P]> : MaybeRef<T[P]>;
   };
 
-  declare type ToRawByRecordRef<T extends Recordable> = {
+  type ToRawByRecordRef<T extends Recordable> = {
     [P in keyof T]: T[P] extends Ref<infer U>
       ? U
       : T[P] extends Recordable
         ? ToRawByRecordRef<T[P]>
         : T[P]
   };
-
 }
+
+export {};
